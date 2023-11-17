ba2f8599631f210064b5fa1a2bc45ff9fc771410
Dhinu C Philip <dhinucphilip1022001@gmail.com>
2023-11-17 06:37:15 -0800

    js file add some style change

--- validation.js
+++ validation.js
@@ -1,22 +1,44 @@
-$(document).ready(function(){
-    $("#fname").validate({
-        rules:{
-            fname:{
-                required:true,
-                
-            },
-            lname:{
-                required:true,
-                email:true
-            }
-        },
-        messages:{
-            fname:{
-                required:"Please enter your name."
-            },
-            lname:{
-                required:"Please enter your email."
-            }
-        }
-    })
-})
+var nameError=document.getElementById("name-error")
+var emailError=document.getElementById("email-error")
+var messageError=document.getElementById("message-error")
+var submitError=document.getElementById("submit-error")
+
+function validateName(){
+    var name=document.getElementById("fname").value;
+
+    if(name.length==0){
+        nameError.innerHTML="Name is required";
+        return false;
+    }
+    if(!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)){
+        nameError.innerHTML="Write your full name";
+        return false;
+    }
+    nameError.innerHTML='<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+function validateEmail(){
+    var email =document.getElementById("lname").value;
+
+    if(email.length==0)
+    {
+        emailError.innerHTML="Email is required";
+        return false;
+    }
+    if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/))
+    {
+        emailError.innerHTML="Invalid email";
+        return false;
+    }
+     emailError.innerHTML='<i class="fa-solid fa-circle-check"></i>';
+     return true;
+}
+
+function validateForm(){
+    if(!validateName() || !validateEmail()){
+        submitError.style.display="display";
+        submitError.innerHTML="Please fix error to submit";
+        setTimeout(function(){submitError.style.display='none';},3000);
+        return false;
+    }
+}
